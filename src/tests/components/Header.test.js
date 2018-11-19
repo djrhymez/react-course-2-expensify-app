f08a5e1ab00e00d79177da45c2acf38d5bd4c692
Daniel Reimer <Daniel.Reimer@neovation.local>
2018-11-19 12:20:15 -0600

    Added redirect authentication functionality based on auth status

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,14 +1,20 @@
 //react-test-renderer
 import React from 'react';
-import Header from '../../components/Header';
+import {Header} from '../../components/Header';
 import { shallow } from 'enzyme';
 
-test('should render Header correctly', () => {
-  const wrapper = shallow(<Header />);
-  expect(wrapper).toMatchSnapshot();
+let startLogout, wrapper;
 
-  // expect(wrapper.find('h1').text()).toBe('Expensify');
-  // const renderer = new ReactShallowRenderer();
-  // renderer.render(<Header/>);
-  // expect(renderer.getRenderOutput()).toMatchSnapshot();
+beforeEach(() => {
+  startLogout = jest.fn();
+  wrapper = shallow(<Header startLogout={startLogout}/>);
+}); 
+
+test('should render Header correctly', () => {
+  expect(wrapper).toMatchSnapshot();
+});
+
+test('should call startLogout on button click', () => {
+  wrapper.find('button').at(0).simulate('click');
+  expect(startLogout).toHaveBeenCalledTimes(1);
 });
--- src/tests/components/LoginPage.test.js
+++ src/tests/components/LoginPage.test.js
@@ -1,8 +1,19 @@
-import LoginPage from '../../components/LoginPage';
+import {LoginPage} from '../../components/LoginPage';
 import React from 'react';
 import { shallow } from 'enzyme';
 
+let wrapper, startLogin;
+
+beforeEach(() => {
+  startLogin = jest.fn();
+  wrapper = shallow(<LoginPage startLogin={startLogin}/>);  
+});
+
 test('should render login page properly', () => {
-  const wrapper = shallow(<LoginPage />);
   expect(wrapper).toMatchSnapshot();
+});
+
+test('should call startLogin on button click', () => {
+  wrapper.find('button').at(0).simulate('click');
+  expect(startLogin).toHaveBeenCalledTimes(1);
 });
