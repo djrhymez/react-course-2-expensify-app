bb8fdfed4c52f13da909186332c96caa1e8a15dd
Daniel Reimer <Daniel.Reimer@neovation.local>
2018-11-19 10:40:50 -0600

    Created base login component and refactored routes to include login

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import {BrowserRouter, Route, Switch, NavLink} from 'react-router-dom';
-import ExpenseDashboardPage from '../components/ExpenseDashboardPage';
 import AddExpensePage from '../components/AddExpensePage';
+import ExpenseDashboardPage from '../components/ExpenseDashboardPage';
 import EditExpensePage from '../components/EditExpensePage';
-import HelpPage from '../components/HelpPage';
-import NotFoundPage from '../components/NotFoundPage';
 import Header from '../components/Header';
+import HelpPage from '../components/HelpPage';
+import LoginPage from '../components/LoginPage';
+import NotFoundPage from '../components/NotFoundPage';
 
 const AppRouter = () => (
   <BrowserRouter>
     <div>
       <Header/>
       <Switch>
-        <Route path="/" component={ExpenseDashboardPage} exact={true}/>
+        <Route path="/" component={LoginPage} exact={true}/>
+        <Route path="/dashboard" component={ExpenseDashboardPage}/>
         <Route path="/create" component={AddExpensePage}/>
         <Route path="/edit/:id" component={EditExpensePage} />
         <Route path="/help" component={HelpPage}/>
